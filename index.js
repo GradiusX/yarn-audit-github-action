ab9944078729e610e0d4726128a81e95be17dce6
GradiusX <francescomifsud@gmail.com>
2023-05-23 22:51:22 +0200

    First Draft

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const exec = require('@actions/exec');
 
-try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
-}
+// Report only this level and above: info|low|moderate|high|critical
+//const severityLevel = core.getInput('severity-level');
+const severityLevel =  "critical";
+
+(async () => {
+    let commandOutput = '';
+    let commandError = '';
+
+    const options = {
+        listeners : {
+            stdout: (data) => {
+                commandOutput += data.toString();
+            },
+            stderr: (data) => {
+                commandError += data.toString();
+            }
+        },
+        silent: true,
+        //ignoreReturnCode: true,
+        cwd : './test'
+    }
+    try{
+        await exec.exec('yarn', ['audit', '--level', severityLevel], options);
+        console.log("No Security Issues found")
+    }catch(error){
+        console.log(commandOutput)
+        core.setFailed(error.message)
+    }
+})();
